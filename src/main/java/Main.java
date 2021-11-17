429d4e373879c171fad4ae57eb6ee4c5d06f1c21
Artur <artur.abramskii@gmail.com>
2021-11-17 17:17:07 +0100

    Main.java überschrieben um Merge-Konflikt zu erzeugen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,1 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Factory factory = new Factory();
-
-        Car car1 = new Car("red", "BMW", 5);
-        Car car2 = new Car("blue", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
-
-        factory.buildCar(car1);
-        factory.buildCar(car2);
-    }
-}
-
+Merge-Konflikt erzeugen
